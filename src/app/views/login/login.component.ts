1913d1ed241ef26a0bcb7c4e4f46e9fff5a1207e
tslacerd <tslacerd@gmail.com>
2022-02-20 18:33:57 -0300

    json e angular para login

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+  }
+
+  loginModel = new User(); 
+
+  mensagem = "";
+
+  onSubmit(){
+    console.log(this.loginModel)
+
+    this.loginService.login(this.loginModel).subscribe( (response) => {
+      console.log("Sucesso!")
+      this.mensagem = "Sucesso"
+    }, (respostaErro) => {
+      this.mensagem = respostaErro.error
+      
+    })    
+  }
+}
